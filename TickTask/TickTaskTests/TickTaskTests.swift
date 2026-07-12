7533bcc6dc498deb9b1bf6b074af4de4535e6e1c
Yug-Patel-1228 <yugcpatel2812@gmail.com>
2026-07-12 13:55:27 +0530

    Created Project.

---
+++ TickTask/TickTaskTests/TickTaskTests.swift
@@ -0,0 +1,19 @@
+//
+//  TickTaskTests.swift
+//  TickTaskTests
+//
+//  Created by Yug on 7/12/26.
+//
+
+import Testing
+@testable import TickTask
+
+struct TickTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
